cd6111f482ec9d97ea2d32aa609b696ac26fd929
raygurudeo <raygurudeo@gmail.com>
2022-06-01 16:59:05 +0530

    Updating useState changes

--- react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import ErrorExample from './tutorial/1-useState/setup/2-useState-basics'
+//import ErrorExample from './tutorial/1-useState/setup/2-useState-basics'
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div>
       <div className='container'>
         <h2>Advanced learning</h2>
       </div>
-      <ErrorExample />
+      <UseStateArray />
     </div>
   )
 }
