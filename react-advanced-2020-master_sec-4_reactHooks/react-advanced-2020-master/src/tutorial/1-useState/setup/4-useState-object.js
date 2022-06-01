816d891b748f0878db6b9ccaa17d59db7736f70d
raygurudeo <raygurudeo@gmail.com>
2022-06-01 18:38:20 +0530

    commiting changes to useState with object files

--- react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 
 // useState hooks with object
 const UseStateObject = () => {
-  const [person, setPerson] = useState(
-    {
-      name:'Guru',
-      age: 26,
-      message: 'Hey'
-    });
+  // const [person, setPerson] = useState(
+  //   {
+  //     name:'Guru',
+  //     age: 26,
+  //     message: 'Hey'
+  //   });
+
+    // We can use multi state value instead of using object directly
+    const [name, setName] = useState('Prince');
+    const [age, setAge] = useState(10);
+    const [message, setMessage] = useState('hey react');
 
     const changeMessage = () => {
       // here we are using spread operator so only message will be updating in the current object and other key
       // and value will not be wiped out by setPerson function.
-      setPerson({...person, message:'Hello'});
+      //setPerson({...person, message:'Hello'});
+
+      setMessage('Hey Prince');
     }
   return (
     <>
-      <h2>{person.name}</h2>
-      <h2>{person.age}</h2>
-      <h2>{person.message}</h2>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{message}</h2>
       <button type='button' className='btn' onClick={changeMessage}>change message</button>
     </>
   );
