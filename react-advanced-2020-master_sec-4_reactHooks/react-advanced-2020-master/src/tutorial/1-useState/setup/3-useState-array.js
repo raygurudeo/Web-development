d61fa79bda4209dae607d9143f4b54b3ffc1a67c
raygurudeo <raygurudeo@gmail.com>
2022-06-01 17:24:10 +0530

    commiting changes to useState files

--- react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,11 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    const restOfPeople = people.filter((person) => person.id !== id);
+    setPeople(restOfPeople);
+  }
   return (
     <>
       {
@@ -11,10 +16,16 @@ const UseStateArray = () => {
           return (
             <div key={id} className='item'>
               <h4>{name}</h4>
+              {/* button to remove particular item */}
+              <button onClick={() => removeItem(id)}>remove</button> 
             </div>
           );
         })
       }
+      {/* button to remove all item */}
+      <button type='button' className='btn' onClick={() => setPeople([])}>  
+        Clear items
+      </button>
     </>
   );
 };
