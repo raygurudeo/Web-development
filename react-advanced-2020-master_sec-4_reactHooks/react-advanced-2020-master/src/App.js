7312234f185cc7011e96ff79a524d7ad6ca994fb
raygurudeo <raygurudeo@gmail.com>
2022-06-01 18:21:38 +0530

    commiting changes to useState with object files

--- react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 //import ErrorExample from './tutorial/1-useState/setup/2-useState-basics'
-import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object'
 function App() {
   return (
     <div>
       <div className='container'>
         <h2>Advanced learning</h2>
       </div>
-      <UseStateArray />
+      <UseStateObject />
     </div>
   )
 }
--- react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020-master_sec-4_reactHooks/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
+// useState hooks with object
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState(
+    {
+      name:'Guru',
+      age: 26,
+      message: 'Hey'
+    });
+
+    const changeMessage = () => {
+      // here we are using spread operator so only message will be updating in the current object and other key
+      // and value will not be wiped out by setPerson function.
+      setPerson({...person, message:'Hello'});
+    }
+  return (
+    <>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+      <button type='button' className='btn' onClick={changeMessage}>change message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
