618335c7cf07e2e57ca169706b5e764e7e286bb7
raygurudeo <raygurudeo@gmail.com>
2022-06-02 20:25:45 +0530

    commiting birthday reminder project changes

--- react-projects/01-birthday-reminder/setup/src/App.js
+++ react-projects/01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h4>{people.length} birthday today</h4>
+        <List people={people}/>
+        <button type='button' onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
