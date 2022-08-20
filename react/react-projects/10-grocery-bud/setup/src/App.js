244b0157b9a518bac3f3506841dbf62b0ac4712c
raygurudeo <raygurudeo@gmail.com>
2022-08-20 08:28:11 +0530

    adding changes

--- react/react-projects/10-grocery-bud/setup/src/App.js
+++ react/react-projects/10-grocery-bud/setup/src/App.js
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+// function to resolve issue with wiping list data once after refresh
+// so, here we are using locaStorage will keep list data
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+}
 function App() {
 
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({show:false, msg:'', type:''});
@@ -23,6 +33,18 @@ function App() {
       showAlert(true, 'please enter the value', 'danger');
     } else if(name && isEditing) {
       // deal with editing
+      setList(
+        list.map((item) => {
+          if(item.id === editId) {
+            return { ...item, title: name};
+          }
+          return item;
+        })
+      )
+      setName('');
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, 'value changed', 'success');
     } else {
       const newItem = {id: new Date().getTime().toString(), title:name};
       setList([...list, newItem]);
@@ -43,6 +65,20 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   }
 
+  // function to edit specific item' first using find() will find item array based on it's id which 
+  // we have clicked then set it to name list and set id to editId
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  }
+
+  // everytime our list gets updated we will add or update value in local storage.
+  // because when we refresh page our list item gets vanished so for time being we are using local storage.
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  },[list]);
   return <section className="section-center">
     <form className="grocery-form" onSubmit={handleClick}>
       {/* removeAltert props will hide alert after 3000ms */}
@@ -58,7 +94,7 @@ function App() {
     </form>
     { list.length > 0 && (
       <div className="grocery-container">
-      <List items={list} removeItem={removeItem} />
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className="clear-btn" onClick={clearItems}>clear items</button>
     </div>
     )}
--- react/react-projects/10-grocery-bud/setup/src/List.js
+++ react/react-projects/10-grocery-bud/setup/src/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items, removeItem}) => {
+const List = ({items, removeItem, editItem}) => {
   return (
   <div className='grocery-list'>
     {items.map((item) => {
@@ -8,7 +8,7 @@ const List = ({items, removeItem}) => {
       return <article className='grocery-item' key={id}>
         <p className='title'>{title}</p>
         <div className="btn-container">
-          <button className="edit-btn">
+          <button className="edit-btn" onClick={() => editItem(id)}>
             <FaEdit />
           </button>
           <button className="delete-btn" onClick={() => removeItem(id)}>
